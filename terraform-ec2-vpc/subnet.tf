34342d9a4b8c07659821349623de8ba4e7598fed
PradnyaJoshi183 <161480544+PradnyaJoshi183@users.noreply.github.com>
2026-02-04 21:06:59 +0530

    Add public subnet resource in subnet.tf

---
+++ terraform-ec2-vpc/subnet.tf
@@ -0,0 +1,8 @@
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = "${var.aws_region}a"
+
+  tags = { Name = "tf-public-subnet" }
+}
